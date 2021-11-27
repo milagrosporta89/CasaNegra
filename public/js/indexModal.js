e8b93a5be56072dae76b5ff795ea9c35ba4d3147
milagrosporta89 <milagrosporta89@gmail.com>
2021-11-27 20:26:47 -0300

    modal andando en index

--- public/js/indexModal.js
+++ public/js/indexModal.js
@@ -1,6 +1,6 @@
 const openModalButtons = document.querySelectorAll("[data-modal-target]") /* apunta a link de mas servicios, no es un boton, es un <a> */
 const closeModalButtons= document.querySelectorAll("[data-close-button]")
-const overlay= document.getElementById("overlay")
+let overlay= document.getElementById("overlay")
 
 console.log(closeModalButtons,"soy close modal")
 console.log(openModalButtons, "soy open modal")
@@ -9,14 +9,27 @@ console.log("funciona")
 openModalButtons.forEach(button=>{
     button.addEventListener("click", () =>{
         const modal = document.querySelector(button.dataset.modalTarget)
-        const modalClose = document.querySelector(button.dataset.closeButton)
-/*         openModal(modal) */
-        console.log (button.dataset)
+        
+        openModal(modal)
+        
     })
 })
-/* function openModal(modal){
+closeModalButtons.forEach(button=>{
+    button.addEventListener("click", () =>{
+        const modal = button.closest(".modal-servicio")
+     
+        closeModal(modal)
+        
+    })
+})
+function openModal(modal){
     if(modal==null) return 
-    
+    modal.classList.add ("active")
     overlay.classList.add("active")
 
-} */
+}
+function closeModal (modal){
+    if(modal==null) return 
+    modal.classList.remove ("active")
+    overlay.classList.remove("active")
+}
